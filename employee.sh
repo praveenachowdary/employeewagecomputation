95c6bd923e4f9ddc31b39743167f948ecb1eb6fc
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-07-24 19:49:57 +0530

    add part time employee wage

--- employee.sh
+++ employee.sh
@@ -4,7 +4,9 @@ isPresent=1;
 randomCheck=$(( RANDOM%2 ));
 if [$isPresent -eq $randomCheck ];
 then
-echo "Employee is Present";
+ empRatePerHr=20;
+	empHrs=8;
+	salary=$(($empHrs*$empRatePerHr));
 else
-echo "Employee is absent";
+	salary=0;
 fi
