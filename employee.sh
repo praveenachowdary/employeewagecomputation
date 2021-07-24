ccc190fd4acdbec21b033c030e30185d766865e6
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-07-24 19:47:04 +0530

    calculate daily employee wage

--- employee.sh
+++ employee.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -x
 echo "Welcome to the employee wage page"
-
-
+isPresent=1;
+randomCheck=$(( RANDOM%2 ));
+if [$isPresent -eq $randomCheck ];
+then
+echo "Employee is Present";
+else
+echo "Employee is absent";
+fi
