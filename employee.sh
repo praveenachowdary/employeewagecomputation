bcee439241e2b39a50bda709cee15672a127aa21
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-07-24 19:58:05 +0530

    solving using switch case

--- employee.sh
+++ employee.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -x
 echo "Welcome to the employee wage page"
-isPresent=1;
-randomCheck=$(( RANDOM%2 ));
-if [$isPresent -eq $randomCheck ];
-then
- empRatePerHr=20;
-	empHrs=8;
-	salary=$(($empHrs*$empRatePerHr));
-else
-	salary=0;
-fi
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$(( RANDOM%3 ));
+
+case $empCheck in
+	$isFullTime)
+					empHrs=8
+					;;
+	$isPartTime)
+					empHrs=4
+					;;
+	*)
+					empHrs=0
+					;;
+esac
+salary=$(( $empHrs*$empRatePerHr ));
